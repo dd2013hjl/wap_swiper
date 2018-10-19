581c6cf1bbdb13562a4d45c2923d686cf4045009
dd2013hjl <1615144427@qq.com>
2018-10-19 14:37:47 +0800

    缩略图
    缩略图

--- src/views/Show.vue
+++ src/views/Show.vue
@@ -1,89 +1,117 @@
 
 <template>
-<div id="wrap">
-<div class="swiper-container">
-<div class="swiper-wrapper">
-<div class="swiper-slide" v-for="(item,index) in imgList" :key="index">
-< img :src="item">
-</div>
-</div>
-</div>
-<div class="thumbs-container">
-<div class="swiper-wrapper">
-<div class="swiper-slide" v-for="item in imgList" :key="item">
-< img :src="item">
-</div>
-</div>
-</div>
-</div>
+  <div id="wrap">
+    <div class="swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide" v-for="(item,index) in imgList" :key="index">
+          <img :src="item">
+        </div>
+      </div>
+    </div>
+    <div class="thumbs-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide" v-for="(item,index) in imgList" :key="index">
+          <!-- <img :src="item"> -->
+          <div class="imgitem" :style="{backgroundImage: 'url('+item+')'}"></div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import Swiper from 'swiper'
+import Swiper from "swiper";
+
 export default {
-data () {
-return {
-imgList: [
-require('assets/image/swiperimg.jpg'),
-require('assets/image/timg.jpg'),
-require('assets/image/timg.jpg'),
-require('assets/image/timg.jpg'),
-require('assets/image/timg.jpg'),
-require('assets/image/timg.jpg'),
-require('assets/image/timg.jpg'),
-require('assets/image/timg.jpg')
-]
-}
-},
-mounted () {
-const thumbSwiper = new Swiper('.thumbs-container', {
-spaceBetween: 10,
-slidesPerView: 4,
-watchSlidesVisibility: true
-})
-const swiper = new Swiper('.swiper-container', {
-spaceBetween: 10,
-loop: true,
-thumbs: {
-swiper: thumbSwiper
-}
-})
-}
-}
+  data() {
+    return {
+      imgList: [
+        require("assets/image/swiperimg.jpg"),
+        require("assets/image/timg.jpg"),
+        require("assets/image/timg.jpg"),
+        require("assets/image/timg.jpg"),
+        require("assets/image/timg.jpg"),
+        require("assets/image/timg.jpg"),
+        require("assets/image/timg.jpg"),
+        require("assets/image/timg.jpg")
+      ]
+    };
+  },
+
+  mounted() {
+    const thumbSwiper = new Swiper(".thumbs-container", {
+      spaceBetween: 10,
+      slidesPerView: 4,
+      watchSlidesVisibility: true
+    });
+
+    const swiper = new Swiper(".swiper-container", {
+      spaceBetween: 10,
+      loop: true,
+      thumbs: {
+        swiper: thumbSwiper
+      }
+    });
+  }
+};
 </script>
 
 <style scoped>
 #wrap {
-position: fixed;
-left: 0;
-right: 0;
-top: 0;
-bottom: 0;
-z-index: 1;
-background: #fff;
-display: flex;
-align-items: center;
-justify-content: center;
-background: #000;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  z-index: 1;
+  background: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #000;
 }
-.swiper-container{
-display: flex;
-align-items: center;
+
+.swiper-container {
+  display: flex;
+  align-items: center;
 }
-.swiper-wrapper{
-align-items: center;
-} 
-.swiper-slide{
-overflow: hidden;
+
+.swiper-wrapper {
+  align-items: center;
 }
-.swiper-slide img{
-width: 100%;
+
+.swiper-slide {
+  overflow: hidden;
 }
-.thumbs-container{
-position: fixed;
-left: 0;
-right: 0;
-bottom: 20px;
-z-index:3;
+
+.swiper-slide img {
+  width: 100%;
+}
+
+.thumbs-container {
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 20px;
+  z-index: 3;
+}
+
+.thumbs-container .imgitem {
+  width: 80px;
+  height: 60px;
+  background-size: cover;
+  background-position: 50% 50%;
+  background-repeat: no-repeat;
+}
+
+.thumbs-container .swiper-slide,
+.thumbs-container .swiper-slide-visible,
+.thumbs-container .swiper-slide-active {
+  margin-right: 5px !important;
+  width: 80px !important;
+}
+.thumbs-container .swiper-slide-active{
+  box-sizing: border-box;
+  border: 1px solid #fff;
 }
 </style>
