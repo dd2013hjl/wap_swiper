d8298f71ab051699ad86ba88da059d980d15bddb
beer-on-ice <li997477295@outlook.com>
2018-10-19 11:23:42 +0800

    弃用vue-awesome-swiper,使用swiper4

--- src/views/Show.vue
+++ src/views/Show.vue
@@ -1,55 +1,72 @@
  <template>
-  <div>
-    <swiper :options="swiperOption" ref="mySwiper" id="mySwiper">
-      <swiper-slide>
-        <img src="https://cache2.vphotos.cn/bkwft01/upload/photoAlbum/paster/4362571b50164948b7659f3e80d5983e.jpeg" alt="" srcset="">
-      </swiper-slide>
-      <swiper-slide>
-        <img src="https://cache2.vphotos.cn/bkwft01/upload/photoAlbum/paster/4362571b50164948b7659f3e80d5983e.jpeg" alt="" srcset="">
-      </swiper-slide>
-      <swiper-slide>
-        <img src="https://cache2.vphotos.cn/bkwft01/upload/photoAlbum/paster/4362571b50164948b7659f3e80d5983e.jpeg" alt="" srcset="">
-      </swiper-slide>
-      <swiper-slide>
-        <img src="https://cache2.vphotos.cn/bkwft01/upload/photoAlbum/paster/4362571b50164948b7659f3e80d5983e.jpeg" alt="" srcset="">
-      </swiper-slide>
-      <swiper-slide>
-        <img src="https://cache2.vphotos.cn/bkwft01/upload/photoAlbum/paster/4362571b50164948b7659f3e80d5983e.jpeg" alt="" srcset="">
-      </swiper-slide>
-      <div class="swiper-button-prev" slot="button-prev"></div>
-      <div class="swiper-button-next" slot="button-next"></div>
-    </swiper>
+  <div id="wrap">
+    <div class="swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide" v-for="item in imgList" :key="item">
+          <img :src="item">
+        </div>
+      </div>
+    </div>
+    <div class="thumbs-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide" v-for="item in imgList" :key="item">
+          <img :src="item">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Swiper from 'swiper'
+
 export default {
   data () {
     return {
-      swiperOption: {
-        loop: true,
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev'
-        }
-      }
+      imgList: [
+        require('assets/image/pum/1.jpg'),
+        require('assets/image/pum/2.jpg'),
+        require('assets/image/pum/3.jpg'),
+        require('assets/image/pum/4.jpg'),
+        require('assets/image/pum/5.jpg'),
+        require('assets/image/pum/6.jpg'),
+        require('assets/image/pum/7.jpg'),
+        require('assets/image/pum/8.jpg')
+      ]
     }
   },
-  computed: {
-    swiper () {
-      return this.$refs.mySwiper.swiper
-    }
+  mounted () {
+    const thumbSwiper = new Swiper('.thumbs-container', {
+      spaceBetween: 10,
+      slidesPerView: 4,
+      watchSlidesVisibility: true
+    })
+    const swiper = new Swiper('.swiper-container', {
+      spaceBetween: 10,
+      loop: true,
+      thumbs: {
+        swiper: thumbSwiper
+      }
+    })
   }
 }
 </script>
 
 <style scoped>
-#mySwiper {
+#wrap {
   position: fixed;
   left: 0;
   right: 0;
   top: 0;
   bottom: 0;
-  z-index: 2;
+  z-index: 1;
+}
+.swiper-container {
+  width: 600px;
+  height: 300px;
+}
+.swiper-slide {
+  width: 100%;
+  height: 400px;
 }
 </style>
